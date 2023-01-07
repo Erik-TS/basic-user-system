c34398652ae818a41eed1f944ca153cf4e1e4f97
erikmint <erik.t3600@hotmail.com>
2023-01-07 11:16:06 -0300

    Create form and implement Sass.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
-export default function Home(){
-    return(
-        <div>
-            <h1>Hello World!</h1>
+export default function Home() {
+    return (
+        <div className={'fullpageContainer'}>
+            <Form />
         </div>
+    )
+}
+
+function handleSubmit(e) {
+    e.preventDefault()
+    const user = {
+        email: e.target.email.value,
+        password: e.target.password.value
+    }
+}
+
+function Form() {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={"formBox"}>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+                <button type="submit">Log In</button>
+            </div>
+        </form>
     )
 }
