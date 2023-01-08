cdb5c289a1efa2724be5dd232c04414cd0a7546a
erikmint <erik.t3600@hotmail.com>
2023-01-08 02:27:34 -0300

    Create UserDashboard component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,33 @@ export default function Home() {
     )
 }
 
+function UserDashboard(props) {
+    return (
+        <div className={"fullpageContainer"}>
+            <div className={"userDashboard"}>
+                <h1>{"User Info".toUpperCase()}</h1>
+                <div className={"col left"}>
+                    <h2>UserId:</h2>
+                    <h2>Username:</h2>
+                    <h2>Email:</h2>
+                    <h2>Birthdate:</h2>
+                    <h2>Registered:</h2>
+                </div>
+                <div className={"col right"}>
+                    <h2>{props.userId}</h2>
+                    <h2>{props.username}</h2>
+                    <h2>{props.email}</h2>
+                    <h2>{props.birthDate}</h2>
+                    <h2>{props.registered}</h2>
+                </div>
+            </div>
+            <div className={"logOut"}>
+                <input type="button" value="Log Out" />
+            </div>
+        </div>
+    )
+}
+
 function handleSubmit(e) {
     e.preventDefault()
     const user = {
